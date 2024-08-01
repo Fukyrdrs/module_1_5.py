bd46011af7b24cce4acaab56d4f2ae7214ccc98d
Fukyrdrs <artm.filipppov@mail.ru>
2024-08-01 12:38:00 +0300

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,8 @@
 immutable_var = 'table', 12, True
 print('Immutable tuple:', immutable_var)
 #immutable_var[1] = 21  Изменить кортеж нельзя, потому что кортеж не поддерживает обращение по элементам
-mutable_list = ['white', 'blue', 'black']
-mutable_list[2] = 'red'
+mutable_list = ['table', 12, True]
+mutable_list[0] = 'chair'
+mutable_list[1] = 221
+mutable_list[2] = False
 print('Mutable list:', mutable_list)
