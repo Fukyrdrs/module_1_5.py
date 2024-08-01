58f0aa5be5240afa453ffc41e4e8019c58d082aa
Fukyrdrs <artm.filipppov@mail.ru>
2024-08-01 11:39:01 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var = 'table', 12, True
+print('Immutable tuple:', immutable_var)
+#immutable_var[1] = 21  Изменить кортеж нельзя, потому что кортеж не поддерживает обращение по элементам
+mutable_list = ['white', 'blue', 'black']
+mutable_list[2] = 'red'
+print('Mutable list:', mutable_list)
